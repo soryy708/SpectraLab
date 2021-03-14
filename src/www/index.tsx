b6ca81f00c07db7538f6ae5f36629eb7984cdb0e
Climax708 <soryy708@gmail.com>
2021-03-14 16:10:54 +0200

    Impl display graph page

--- src/www/index.tsx
+++ src/www/index.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDom from 'react-dom';
 import { HashRouter, Route, Switch } from 'react-router-dom';
+import GraphRoute from './page/graph';
 import LoadRoute from './page/load';
+import Matrix from '../matrix';
 
 function App() {
+    const [matrix/*, setMatrix*/] = useState<Matrix>();
+
     return <HashRouter>
         <Switch>
             <Route exact path="/" component={LoadRoute}/>
+            <Route path="/graph" component={() => <GraphRoute data={matrix}/>}/>
         </Switch>
     </HashRouter>;
 }
---
+++ src/www/page/graph.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import Graph from '../components/graph';
+import Toggle from '../components/toggle';
+import Matrix from '../../matrix';
+
+type GraphPageProps = {
+    data?: Matrix;
+};
+
+const GraphPage: React.FunctionComponent<GraphPageProps> = (props: GraphPageProps) => {
+    const [showLocalExtremum, setShowLocalExtremum] = useState(false);
+    const [showGlobalExtremum, setShowGlobalExtremum] = useState(false);
+
+    return <div className="page graphPage">
+        <div className="leftPart">
+            <Toggle
+                label="Show local extremum?"
+                value={showLocalExtremum}
+                onChange={(newVal) => setShowLocalExtremum(newVal)}
+            />
+            <Toggle
+                label="Show global extremum?"
+                value={showGlobalExtremum}
+                onChange={(newVal) => setShowGlobalExtremum(newVal)}
+            />
+        </div>
+        <div className="rightPart">
+            <Graph
+                data={props.data}
+            />
+        </div>
+    </div>;
+};
+
+export default GraphPage;
