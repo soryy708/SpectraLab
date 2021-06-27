e6bc6766dfc4606ffc531b46b0868045fb1525ce
Climax708 <soryy708@gmail.com>
2021-06-27 18:38:24 +0300

    Use matrix library from npm

--- src/matrix.ts
+++ src/matrix.ts
@@ -1,7 +1,9 @@
-class Matrix {
+import { Matrix } from 'ml-matrix';
+
+class MyMatrix {
     private sizeX = NaN;
     private sizeY = NaN;
-    private data: Array<Array<number>> = null;
+    private data: Matrix = null;
 
     constructor(data: Array<Array<number>>) {
         const sizeMin = data.reduce((min, cur) => cur.length < min ? cur.length : min, +Infinity);
@@ -10,7 +12,7 @@ class Matrix {
             throw (new Error('Non-rectangular'));
         }
 
-        this.data = data;
+        this.data = new Matrix(data);
         this.sizeY = data.length;
         this.sizeX = sizeMax;
     }
@@ -28,11 +30,11 @@ class Matrix {
             throw new Error('Out of range');
         }
 
-        return this.data[y][x];
+        return this.data.get(y, x);
     }
 
     forEach(cb: (val: number, index: [x: number, y: number]) => void): void {
-        this.data.forEach((row, y) => {
+        this.data.to2DArray().forEach((row, y) => {
             row.forEach((val, x) => {
                 cb(val, [x, y]);
             });
@@ -40,11 +42,11 @@ class Matrix {
     }
 
     toArray(): number[] {
-        return [...this.data.reduce((prev, cur) => [...prev, ...cur], [])];
+        return this.data.to1DArray();
     }
 
     toMultiDimensionalArray(): Array<number[]> {
-        return [...this.data.map(arr => [...arr])];
+        return this.data.to2DArray();
     }
 
     neighborIndexesOf(x: number, y: number): [number, number][] {
@@ -101,74 +103,37 @@ class Matrix {
         return offsets.map(([offX, offY]) => [x + offX, y + offY]);
     }
 
-    getColumn(index: number): Matrix {
+    getColumn(index: number): MyMatrix {
         if (index < 0 || index >= this.sizeX) {
             throw new Error('Out of range');
         }
 
-        const vector: Array<number> = [];
-        for (let i = 0; i < this.sizeY; ++i) {
-            vector.push(this.data[i][index]);
-        }
-        return new Matrix(vector.map(scalar => [scalar]));
+        return new MyMatrix(this.data.getColumn(index).map(scalar => [scalar]));
     }
 
-    getRow(index: number): Matrix {
+    getRow(index: number): MyMatrix {
         if (index < 0 || index >= this.sizeY) {
             throw new Error('Out of range');
         }
 
-        const vector: Array<number> = [];
-        for (let i = 0; i < this.sizeX; ++i) {
-            vector.push(this.data[index][i]);
-        }
-        return new Matrix([vector]);
+        return new MyMatrix([this.data.getRow(index)]);
     }
 
-    dotProduct(other: Matrix): Matrix {
+    dotProduct(other: MyMatrix): MyMatrix {
         if (this.sizeX !== other.sizeY) {
             throw new Error('Bad size');
         }
 
-        const newData: Array<Array<number>> = [];
-        for (let myRow = 0; myRow < this.sizeY; ++myRow) {
-            const row: Array<number> = [];
-            for (let otherColumn = 0; otherColumn < other.sizeX; ++otherColumn) {
-                let sum = 0;
-                // Worth noting: this.sizeX == other.sizeY
-                for (let i = 0; i < this.sizeX; ++i) {
-                    sum += this.data[myRow][i] * other.data[i][otherColumn];
-                }
-                row.push(sum);
-            }
-            newData.push(row);
-        }
-        return new Matrix(newData);
+        return new MyMatrix(this.data.mmul(other.data).to2DArray());
     }
 
-    mulScalar(scalar: number): Matrix {
-        const newData: Array<Array<number>> = [];
-        for (let i = 0; i < this.sizeY; ++i) {
-            const row: Array<number> = [];
-            for (let j = 0; j < this.sizeX; ++j) {
-                row.push(this.data[i][j] * scalar);
-            }
-            newData.push(row);
-        }
-        return new Matrix(newData);
+    mulScalar(scalar: number): MyMatrix {
+        return new MyMatrix(Matrix.mul(this.data, scalar).to2DArray());
     }
 
-    transpose(): Matrix {
-        const newData: Array<Array<number>> = [];
-        for (let i = 0; i < this.sizeX; ++i) {
-            const row: Array<number> = [];
-            for (let j = 0; j < this.sizeY; ++j) {
-                row.push(this.data[j][i]);
-            }
-            newData.push(row);
-        }
-        return new Matrix(newData);
+    transpose(): MyMatrix {
+        return new MyMatrix(this.data.transpose().to2DArray());
     }
 }
 
-export default Matrix;
+export default MyMatrix;
